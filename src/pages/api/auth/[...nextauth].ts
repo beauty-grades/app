9b53943f45ad8f06dfccbeff8c2e66e5394e0f98
Anthony Cueva <hi.cuevantngmail.com>
2023-03-25 14:33:52 -0500

    trying to get the nextauth to work with the api routes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import { XataAdapter } from "@next-auth/xata-adapter"
 import NextAuth from "next-auth"
-import EmailProvider from "next-auth/providers/email"
 import GoogleProvider from "next-auth/providers/google"
 
 import Xata from "@/lib/xata"
@@ -10,17 +9,6 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
     }),
   ],
   adapter: XataAdapter(Xata),
