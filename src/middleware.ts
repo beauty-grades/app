12a7150620eb5f8af067c1cd5ad1575a0f103629
Anthony Cueva <hi.cuevantngmail.com>
2023-03-25 23:46:19 -0500

    prueba de fuego

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,69 +14,61 @@ export async function middleware(req: NextRequest, event: NextFetchEvent) {
       : "next-auth.session-token"
   )?.value
 
-  event.waitUntil(
-    (async () => {
-      if (session_token) {
-        const raw_session = await Xata.db.nextauth_sessions
-          .select(["*", "user.email"])
-          .filter({ sessionToken: session_token })
-          .getFirst()
+  if (session_token) {
+    const raw_session = await Xata.db.nextauth_sessions
+      .select(["*", "user.email"])
+      .filter({ sessionToken: session_token })
+      .getFirst()
 
-        const email = raw_session?.user?.email
+    const email = raw_session?.user?.email
 
-        if (email) {
-          const user = await Xata.db.student
-            .select(["*"])
-            .filter({ email })
-            .getFirst()
+    if (email) {
+      const user = await Xata.db.student
+        .select(["*"])
+        .filter({ email })
+        .getFirst()
 
-          // continue only if the token was stored recently (less than 4 hours ago)
-          if (
-            user?.utec_token &&
-            user.last_token_stored_at &&
-            new Date().getTime() - user.last_token_stored_at.getTime() <
-              4 * 60 * 60 * 1000
-          ) {
-            // populate at most once every 24 hours
-            if (
-              !user.last_populated_at ||
-              new Date().getTime() - user.last_populated_at.getTime() >
-                24 * 60 * 60 * 1000
-            ) {
-              if (user.populating) {
-                console.log("User", email, "is already populating. Skipping...")
-              } else {
-                console.log("Populating user", email)
-                await user.update({
-                  populating: true,
-                })
-                try {
-                  await populate(user.utec_token as string, email)
-                } catch (e) {
-                  console.error("Error populating user", email)
-                  console.error(e)
-                }
-                await user.update({
-                  last_populated_at: new Date(),
-                  populating: false,
-                })
-                console.log("Finished populating user", email)
-              }
-            } else {
-              console.log("Skipping population for user", email)
-            }
+      // continue only if the token was stored recently (less than 4 hours ago)
+      if (
+        user?.utec_token &&
+        user.last_token_stored_at &&
+        new Date().getTime() - user.last_token_stored_at.getTime() <
+          4 * 60 * 60 * 1000
+      ) {
+        // populate at most once every 24 hours
+        if (
+          !user.last_populated_at ||
+          new Date().getTime() - user.last_populated_at.getTime() >
+            24 * 60 * 60 * 1000
+        ) {
+          if (user.populating) {
+            console.log("User", email, "is already populating. Skipping...")
           } else {
-            console.log("No valid token found for user", email)
+            event.waitUntil(
+              fetch("https://beauty-grades-api.up.railway.app/api/populate", {
+                method: "POST",
+                headers: {
+                  Authorization: user.utec_token,
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email }),
+              })
+            )
+            return NextResponse.next()
           }
         } else {
-          console.log("No email found for session token", session_token)
+          console.log("Skipping population for user", email)
         }
       } else {
-        console.log("No session token found")
+        console.log("No valid token found for user", email)
       }
-    })()
-  )
-  
+    } else {
+      console.log("No email found for session token", session_token)
+    }
+  } else {
+    console.log("No session token found")
+  }
+
   return NextResponse.next()
 }
 
